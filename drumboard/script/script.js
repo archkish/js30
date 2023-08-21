7b10e52aeb7b7cefefbf1638df4d8754679ce2f1
arch <kisharchi@gmail.com>
2023-08-21 22:45:01 +0200

    add drumBoard

---
+++ drumboard/script/script.js
@@ -0,0 +1,27 @@
+
+function playSound(e) {
+    let audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    let key = document.querySelector(`div[data-key="${e.key}"]`)
+    
+    if(!audio) return
+
+    audio.currentTime = 0
+    audio.play()
+
+    key.classList.add('playing')
+
+    // setTimeout(() => {
+    //     key.classList.remove('playing')
+    // }, "100")
+    
+}
+
+function removeTransition (e) {
+    if(e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+let keys = document.querySelectorAll('.key')
+keys.forEach(key => 
+    key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound)
